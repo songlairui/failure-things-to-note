26506a1c04f2641e0aa04edfecdaf9d31903e296
宋来瑞 <cnre@live.cn>
2019-11-25 16:01:36 +0800

    feat(common): useFetch

---
+++ renderer/common/useFetch.ts
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+import fetch from "isomorphic-unfetch";
+
+const fullUrl = (url: string) => `http://localhost:7632/api/${url}`;
+
+export default function useFetch<T>(
+  shortUrl: string,
+  options?: RequestInit,
+  initVal?: any
+): [Error | undefined, T] {
+  const url = fullUrl(shortUrl);
+  const [data, setData] = useState<T>(initVal as T);
+  const [error, setError] = useState<Error>();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch(url, options);
+        const json = await res.json();
+
+        setData(json);
+      } catch (error) {
+        setError(error);
+      }
+    };
+    fetchData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [url]);
+
+  return [error, data];
+}
